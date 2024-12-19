38278b000010bf91e785c7c0dcc2d5a46057e228
manasa-manoj-nbr <manasa23bcy41@iiitkottayam.ac.in>
2024-12-19 12:28:03 +0530

    header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     content: [
-      './index.html',
-      './src/**/*.{html,js}',
+        './index.html',
+        './src/**/*.{html,js}',
     ],
-    // ...
-  }
+    theme: {
+        extend: {
+            fontFamily: {
+                'serifText': ['"DM Serif Text"', 'serif'],
+                'inter' : ['Inter', 'sans-serif'],
+              },
+            backgroundImage: {
+                'chat': 'linear-gradient(135deg, #0a0f2e, #34113b)',
+            },
+        },
+    }
+}
