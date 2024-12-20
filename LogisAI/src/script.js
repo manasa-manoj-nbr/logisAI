7277d8b2a3f712d09b165f8bbd7a715aee3901ae
manasa-manoj-nbr <manasa23bcy41@iiitkottayam.ac.in>
2024-12-20 09:02:37 +0530

    added the functionalities

---
+++ LogisAI/src/script.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const chatMessages = document.getElementById("chatMessages");
+    const chatForm = document.getElementById("chatForm");
+    const userInput = document.getElementById("userInput");
+    const sendButton = document.getElementById("sendButton");
+
+    userInput.addEventListener("input", () => {
+        userInput.style.width = "auto";
+        userInput.style.width = userInput.scrollHeight + "px";
+    });
+
+    chatForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const message = userInput.value.trim();
+        if (!message) return;
+        const user = addMessage(message, true);
+        userInput.value = ""
+        userInput.style.height = "auto";
+        sendButton.disabled = true;
+        const typing = typeLoading()
+        try {
+            const response = await generateResponse(message);
+            typing.remove()
+            addMessage(response,false)
+        } catch (error) {
+            typing.remove()
+            const errorMessage = addErrorMessage(error.message);
+        } finally {
+            sendButton.disabled=false
+        }
+    })
+
+    async function generateResponse(prompt) {
+        const response = await fetch(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyA-PexBwPpzu_-xAkwnOTM306CKDbS2Kvw`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                contents: [
+                    {
+                        parts: [
+                            {
+                                text: prompt
+                            }
+                        ]
+                    }
+                ]
+            }
+            )
+        }
+
+        );
+        if (!response.ok) {
+            throw new Error("Failed to generate response. Please try again later!");
+        }
+        const data = await response.json();
+        return data.candidates[0].content.parts[0].text;
+    };
+
+    function addMessage(text,isUser) {
+        const message = document.createElement("div");
+        message.className = `message ${isUser ? "user-message" : ""}`;
+        message.innerHTML = `
+        <div class = "avatar ${isUser ? 'user-avatar' : ""}"> ${isUser ? "U" : "AI"} </div>
+        <div class="message-content">${text}</div>
+        `;
+        chatMessages.appendChild(message);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+
+    function typeLoading() {
+        const indicator = document.createElement("div");
+        indicator.className = "message";
+        indicator.innerHTML = `
+        <div class="avatar">AI</div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        `;
+        chatMessages.appendChild(indicator);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+        return indicator;
+    };
+
+    function addErrorMessage(text) {
+        const error = document.createElement("div");
+        error.className = "message";
+        error.innerHTML = `
+                <div class="avatar">AI</div>
+                <div class="message-content" style="color:red;">Error ${text}</div>
+        `;
+        chatMessages.appendChild(error);
+        return error;
+    }
+});
--- LogisAI/tailwind.config.js
+++ LogisAI/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
               },
             backgroundImage: {
                 'chat': 'linear-gradient(135deg, #0a0f2e, #34113b)',
+                'chat-btn': 'linear-gradient(to right, #2c335e, #4f135b)',
+                'chat-btn-hvr': 'linear-gradient(to right,#4f135b,#2c335e)',
             },
         },
     }
